8e99b75de4abf53b53fb2fb88dafc0b4cec633d7
Hind AlRamah <50259393+Hindram@users.noreply.github.com>
2021-02-05 01:28:46 +0300

    Update score.py

--- nd00333-capstone-master/starter_file/score.py
+++ nd00333-capstone-master/starter_file/score.py
@@ -10,15 +10,15 @@ import joblib
 
 def init():
     global model
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'model.joblib')
-    model = joblib.load(model_path)
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'model.pkl')
     
-#     try:
-#         model = joblib.load(model_path)
-#         logger.info("Loaded successfully...")
-#     except Exception as e:
-#         error = str(e)
-#         return error
+    
+    try:
+        model = joblib.load(model_path)
+        logger.info("Loaded successfully...")
+    except Exception as e:
+        error = str(e)
+        return error
      
 def run(data):
     try:      
